f82f5d7fbd7fcc2a25036317de75c110f7e98311
Michal Kononenko <michalkononenko@gmail.com>
2026-03-14 20:01:39 -0400

    Got the first sphinxcontrib_nixdomain build working

--- flake.nix
+++ flake.nix
@@ -15,28 +15,60 @@
 
   outputs =
     { nixpkgs, home-manager, disko, ... }@inputs:
-    {
-      nixosConfigurations = {
-        artax = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./configurations/artax
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.mkononenko = ./home/mkononenko/user.nix;
-            }
+      let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        sphinxcontrib-nixdomain = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "sphinxcontrib-nixdomain";
+          version = "0.1.4";
+          pyproject = true;
+          build-system = [ pkgs.python3Packages.hatchling ];
+          propagatedBuildInputs = [ 
+            pkgs.python3Packages.sphinx 
+            pkgs.python3Packages.pydantic
           ];
+          src = pkgs.fetchFromGitHub {
+            owner = "minijackson";
+            repo = "sphinxcontrib-nixdomain";
+            rev = "v0.1.4";
+            hash = "sha256-dyXEyVTi2bfX+LlIpnx39DlEVk5mvbJ9cBY9O+MzJAo=";
+          };
         };
-        tianma1 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            disko.nixosModules.disko
-            ./configurations/tianma1
-          ];
+
+        sphinxEnv = pkgs.python3.withPackages (
+          ps: [
+            ps.sphinx
+            sphinxcontrib-nixdomain
+          ]
+        );
+      in {
+        nixosConfigurations = {
+          artax = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            modules = [
+              ./configurations/artax
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.mkononenko = ./home/mkononenko/user.nix;
+              }
+            ];
+          };
+
+          tianma1 = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            modules = [
+              disko.nixosModules.disko
+              ./configurations/tianma1
+            ];
+          };
+        };
+
+        devShells.${system}.docs = pkgs.mkShell {
+          packages = [ sphinxEnv pkgs.gnumake ];
         };
       };
-    };
 }
 
